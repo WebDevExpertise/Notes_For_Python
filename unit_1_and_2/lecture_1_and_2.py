57b4eb2dffbda35500a9ace8dfc7e2806cf3ac81
WebDevExpertise <88412163+WebDevExpertise@users.noreply.github.com>
2021-08-29 21:48:35 -0400

    Create lecture_1_and_2.py
    Here are the notes for units 1 and 2 of Python

---
+++ unit_1_and_2/lecture_1_and_2.py
@@ -0,0 +1,64 @@
+# Unit 1: Print statements
+
+print('hello world') # prints "hello world" onto the console/terminal
+
+# Unit 2
+    # 2.1 - Creating Variables
+
+x = 5 # variable name, then = sign, then data/content you want the variable to store
+
+print(x) # prints 5
+
+# The data types in Python include: Integers/Whole Numbers, Floating-Point Numbers, Booleans, Strings
+
+    # 2.2 - Reassigning a variable
+
+x = 10
+
+print(x) # prints 10 instead of 5
+
+    # 2.3 & 2.7 - f strings
+
+car = 'bmw'
+
+print(f'I recently got a {car}') # prints "I recently got a bmw" onto the consoloe/terminal
+
+    # 2.4 - String methods
+
+name = 'John doe'
+
+print(name.upper()) # prints "JOHN  DOE" onto the console/terminal. The .upper() turns all the letters in the string to uppercase letters
+
+print(name.lower()) # prints "john doe" onto the console/terminal. The .lower() turns all the letters in the string to lowercase letters
+
+print(name.title()) # prints "John Doe" onto the console/terminal. The .title() turns the first letter in every word to an uppercase letter
+
+    # 2.5 - Printing quotation marks
+ & 
+print('Joe put "A" as his answer on the test') # prints 'Joe put "A" as his answer on the test' onto the console/terminal
+
+    # 2.6 - Operations
+
+# addition
+
+print(10 + 2) # prints 12 onto the console/terminal
+
+#subtraction
+
+print(10 - 2) # prints 8 onto the console/terminal
+
+#multiplication
+
+print(10 * 2) # prints 20 onto the console/terminal
+
+# division
+
+print(10 / 2) # prints 5 onto the console/terminal
+
+#modulus
+
+print(10 / 3) # prints 1 onto the console/terminal
+
+# finding the power of a number
+
+print(10 ** 3) # prints 1000 onto the console/terminal
